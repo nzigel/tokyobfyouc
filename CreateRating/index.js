04170f571a693a767e15452f406f9aeea44c4fb9
Nigel Parker <nzigel@users.noreply.github.com>
2018-10-23 17:13:05 +0900

    updates to include user check and product check and remove async

--- CreateRating/index.js
+++ CreateRating/index.js
@@ -1,18 +1,90 @@
-module.exports = async function (context, req) {
+var request = require('request');
+var rp = require('request-promise-native');
+
+var docObj = {
+    "userId": "",
+    "productId": "",
+    "timestamp": new Date().toJSON().toString(),
+    "locationName": "",
+    "rating": 5,
+    "userNotes": ""
+}
+
+module.exports = function (context, req) {
 
     if (req.body && req.body.userId && req.body.productId && req.body.locationName && req.body.rating && req.body.userNotes) {
 
-        context.bindings.ratingsDocument = req.body;
+        //context.bindings.ratingsDocument = req.body;
 
-        // Success.
-        context.res = {
-            status: 200
+        var usrOptions = {
+            uri: 'https://serverlessohlondonuser.azurewebsites.net/api/GetUser',
+            qs: {
+                userId: req.body.userId // -> uri + '?key=value'
+            },
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true // Automatically parses the JSON string in the response
         };
+
+        var productOptions = {
+            uri: 'https://serverlessohlondonproduct.azurewebsites.net/api/GetProduct',
+            qs: {
+                productId: req.body.productId // -> uri + '?key=value'
+            },
+            headers: {
+                'User-Agent': 'Request-Promise'
+            },
+            json: true // Automatically parses the JSON string in the response
+        };
+
+        docObj.userId = req.body.userId;
+        docObj.productId = req.body.productId;
+        docObj.locationName = req.body.locationName;
+        docObj.rating = req.body.rating;
+        docObj.userNotes = req.body.userNotes;
+
+        rp(usrOptions)
+            .then(function (user) {
+                if (docObj.userId==user.userId) {
+                    // we have matched the user Id
+                    console.log('User name', user.userName);
+                    rp(productOptions)
+                        .then(function (product) {
+                            if (docObj.productId==product.productId) {
+                                // we have matched the product Id
+                                console.log('productName', product.productName);
+                                console.log('productDescription', product.productDescription);
+                                context.bindings.ratingsDocument = docObj;
+                                context.res = {
+                                    status: 200,
+                                    body: JSON.stringify(docObj)
+                                };
+                                context.done();
+                            }
+                            else {
+                                context.res = {
+                                    status: 400,
+                                    body: "productId "+docObj.productId+" not found"
+                                };
+                                context.done();
+                            }
+                        });
+                }
+                else {
+                    context.res = {
+                        status: 400,
+                        body: "userId "+docObj.userId+" not found"
+                    };
+                    context.done();
+                }
+            });
     }
     else {
         context.res = {
             status: 400,
-            body: "The query options 'name' and 'task' are required."
+            body: "Please post a userId, productId, locationName, rating and userNotes in the request body"
         };
+        context.done();
     }
 };
